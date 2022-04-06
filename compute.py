47bcce385a4abcc684b60a99893702ea19e22444
Guillaume <guillaume.delporte52@gmail.com>
2022-04-06 21:08:10 +0200

    finished

--- compute.py
+++ compute.py
@@ -1,4 +1,9 @@
+import math
+
 import numpy as np
+
+from meca_flu_2022.circu import circu
+from meca_flu_2022.force import force
 
 
 def computecl(dom, h, Qin, f):
@@ -58,16 +63,151 @@ def computecl(dom, h, Qin, f):
     if f > 0:
         cl = f * Qin
         CL[35: 108, 10: 42] = cl
-        CL[36: 107, 10: 41] = 0
+        CL[36: 107, 11: 41] = 0
 
     return CL
 
 
 def computecircu(dom, u, v, h):
-    c = 0
-    return c
+    x, y = [], []
+
+    x.extend(np.arange(1, 21, 1))
+    x.extend(np.ones(13) * 21)
+    x.extend(np.arange(22, 180))
+    x.extend(np.ones(18) * 180)
+    x.extend(np.arange(181, 200))
+    x.extend(np.ones(16) * 200)
+    x.extend(np.arange(199, 178, -1))
+    x.extend(np.ones(18) * 180)
+    x.extend(np.arange(179, 21, -1))
+    x.extend(np.ones(13) * 21)
+    x.extend(np.arange(20, 0, -1))
+    x.extend(np.ones(26) * 1)
+
+    y.extend(np.ones(20) * 13)
+    y.extend(np.arange(13, 0, -1))
+    y.extend(np.ones(158) * 1)
+    y.extend(np.arange(1, 19))
+    y.extend(np.ones(19) * 18)
+    y.extend(np.arange(18, 34))
+    y.extend(np.ones(21) * 33)
+    y.extend(np.arange(33, 51))
+    y.extend(np.ones(158) * 50)
+    y.extend(np.arange(50, 37, -1))
+    y.extend(np.ones(20) * 38)
+    y.extend(np.arange(38, 12, -1))
+
+    U, V = np.zeros(len(x)), np.zeros(len(x))
+
+    for i in range(len(x) - 1):
+        U[i] = u[int(x[i]), int(y[i])]
+        V[i] = v[int(x[i]), int(y[i])]
+
+    return h * circu(U, V, x, y)
 
 
 def computeforce(dom, p, Qin):
-    fx, fy = 0, 0
+    x = np.arange(35, 108, 1, dtype=int)
+    y1 = np.ones(73, dtype=int)
+    y1[:] *= 10
+    y2 = np.ones(73, dtype=int)
+    y2[:] *= 41
+
+    y = np.arange(10, 42, 1, dtype=int)
+    x1 = np.ones(32, dtype=int)
+    x1[:] = 35
+    x2 = np.ones(32, dtype=int)
+    x2[:] = 107
+
+    portancey1, portancey2 = np.zeros(len(x)), np.zeros(len(x))
+    for i in range(len(x) - 1):
+        portancey1[i] = p[x[i], y1[i]]
+        portancey2[i] = p[x[i], y2[i]]
+
+    portancex1, portancex2 = np.zeros(len(y)), np.zeros(len(y))
+    for i in range(len(y) - 1):
+        portancex1[i] = p[x1[i], y[i]]
+        portancex2[i] = p[x2[i], y[i]]
+
+    fy1, foo = force(portancey1, x, y1)
+    fy2, foo = force(portancey2, x, y2)
+
+    foo, fx1 = force(portancex1, x1, y)
+    foo, fx2 = force(portancex2, x2, y)
+
+    fx = (fx1 - fx2) * np.sign(Qin)
+    fx = math.copysign(fx, Qin)
+
+    fy = fy1 - fy2
+
     return fx, fy
+
+    # for i in range(0, 20):
+    #     x[i] = i + 1
+    #     y[i] = 13
+    #
+    # incr = 13
+    # for i in range(20, 33):
+    #     x[i] = 21
+    #     y[i] = incr
+    #     incr -= 1
+    #
+    # incr = 21
+    # for i in range(33, 193):
+    #     x[i] = incr
+    #     incr += 1
+    #     y[i] = 1
+    #
+    # incr = 1
+    # for i in range(193, 211):
+    #     x[i] = 180
+    #     y[i] = incr
+    #     incr += 1
+    #
+    # incr = 180
+    # for i in range(211, 232):
+    #     x[i] = incr
+    #     incr += 1
+    #     y[i] = 18
+    #
+    # incr = 18
+    # for i in range(232, 248):
+    #     x[i] = 200
+    #     y[i] = incr
+    #     incr += 1
+    #
+    # incr = 200
+    # for i in range(248, 268):
+    #     x[i] = incr
+    #     incr -= 1
+    #     y[i] = 33
+    #
+    # incr = 33
+    # for i in range(269, 287):
+    #     x[i] = 187
+    #     y[i] = incr
+    #     incr += 1
+    #
+    # incr = 180
+    # for i in range(287, 447):
+    #     x[i] = incr
+    #     incr -= 1
+    #     y[i] = 50
+    #
+    # incr = 50
+    # for i in range(447, 460):
+    #     x[i] = 21
+    #     y[i] = incr
+    #     incr -= 1
+    #
+    # incr = 21
+    # for i in range(460, 480):
+    #     x[i] = incr
+    #     incr -= 1
+    #     y[i] = 38
+    #
+    # incr = 38
+    # for i in range(480, 506):
+    #     x[i] = 1
+    #     y[i] = incr
+    #     incr -= 1
--- main.py
+++ main.py
@@ -7,98 +7,100 @@ from meca_flu_2022.pressure import pressure
 
 if __name__ == "__main__":
 
-    case = 4
+    nbr_case = [1, 2, 3, 4, 5]
     rho = 1000
     x_gp = 2
     y_gp = 2
     Qin = (10 * x_gp + 5 * y_gp) * (10 ** -3)
     p = np.zeros((2, 2))
     p[:] = np.nan
+    for case in nbr_case:
+        if case == 1:
+            dom = np.loadtxt('1-dom.txt', delimiter='\t')
+            num = np.loadtxt('1-num.txt', delimiter='\t')
+            h = 0.5
+            cl = np.loadtxt('1-cl.txt', delimiter='\t')
 
-    if case == 1:
-        dom = np.loadtxt('1-dom.txt', delimiter='\t')
-        num = np.loadtxt('1-num.txt', delimiter='\t')
-        h = 0.5
-        cl = np.loadtxt('1-cl.txt', delimiter='\t')
+        elif case == 2:
+            dom = np.loadtxt('2-dom.txt', delimiter='\t')
+            num = np.loadtxt('2-num.txt', delimiter='\t')
+            h = 0.01
+            F = -1
+            cl = computecl(dom, h, Qin, F)
 
-    elif case == 2:
-        dom = np.loadtxt('2-dom.txt', delimiter='\t')
-        num = np.loadtxt('2-num.txt', delimiter='\t')
-        h = 0.01
-        F = -1
-        cl = computecl(dom, h, Qin, F)
+        elif case == 3:
+            dom = np.loadtxt('2-dom.txt', delimiter='\t')
+            num = np.loadtxt('2-num.txt', delimiter='\t')
+            h = 0.01
+            F = -1
+            Qin = -Qin
+            cl = computecl(dom, h, Qin, F)
 
-    elif case == 3:
-        dom = np.loadtxt('2-dom.txt', delimiter='\t')
-        num = np.loadtxt('2-num.txt', delimiter='\t')
-        h = 0.01
-        F = -1
-        Qin = -Qin
-        cl = computecl(dom, h, Qin, F)
+        elif case == 4:
+            dom = np.loadtxt('3-dom.txt', delimiter='\t')
+            num = np.loadtxt('3-num.txt', delimiter='\t')
+            h = 0.01
+            F = 0.5
+            cl = computecl(dom, h, Qin, F)
 
-    elif case == 4:
-        dom = np.loadtxt('3-dom.txt', delimiter='\t')
-        num = np.loadtxt('3-num.txt', delimiter='\t')
-        h = 0.01
-        F = 0.5
-        cl = computecl(dom, h, Qin, F)
+        else:
+            dom = np.loadtxt('3-dom.txt', delimiter='\t')
+            num = np.loadtxt('3-num.txt', delimiter='\t')
+            h = 0.01
+            F = (15 + 0.3 * 22) / 100
+            cl = computecl(dom, h, Qin, F)
 
-    else:
-        dom = np.loadtxt('3-dom.txt', delimiter='\t')
-        num = np.loadtxt('3-num.txt', delimiter='\t')
-        h = 0.01
-        F = 15 + 0.3 * 22
-        cl = computecl(dom, h, Qin, F)
+        psi = Laplace.solver(dom, num, cl)
+        u, v, s = vel.velocity(psi, dom, h)
 
-    psi = Laplace.solver(dom, num, cl)
-    u, v, s = vel.velocity(psi, dom, h)
+        if case > 1:
+            p = pressure(dom, s, rho)
+            c = computecircu(dom, u, v, h)
+            print('circulation = ', c)
+            fx, fy = computeforce(dom, p, Qin)
+            print('fx = ', fx, 'fy = ', fy)
 
-    if case > 1:
-        p = pressure(dom, s, rho)
-        c = computecircu(dom, u, v, h)
-        fx, fy = computeforce(dom, p, Qin)
+            u = u.T
+            v = v.T
+            s = s.T
+            p = p.T
+            psi = psi.T
 
-        u = u.T
-        v = v.T
-        s = s.T
-        p = p.T
-        psi = psi.T
+        fig, axs = plt.subplots(3, 2, dpi=250)
 
-    fig, axs = plt.subplots(3, 2, dpi=250)
+        fig.suptitle('Cas numéro ' + str(case))
 
-    fig.suptitle(case)
+        axs[0, 0].pcolor(u, cmap='inferno')
+        axs[0, 0].set_title('Horizontal Velocity')
 
-    axs[0, 0].pcolor(u)
-    axs[0, 0].set_title('Horizontal Velocity')
+        axs[1, 0].pcolor(v, cmap='inferno')
+        axs[1, 0].set_title('Vertical Velocity')
 
-    axs[1, 0].pcolor(v)
-    axs[1, 0].set_title('Vertical Velocity')
+        axs[2, 0].pcolor(s, cmap='magma')
+        axs[2, 0].set_title('Mean Velocity')
 
-    axs[2, 0].pcolor(s)
-    axs[2, 0].set_title('Mean Velocity')
+        axs[0, 1].quiver(u, v)
+        axs[0, 1].set_title('Velocity Field')
 
-    axs[0, 1].quiver(u, v)
-    axs[0, 1].set_title('Velocity Field')
+        if case > 1:
+            x = np.arange(1, 203)
+            y = np.arange(1, 53)
+        else:
+            x = np.arange(1, 6)
+            y = np.arange(1, 6)
 
-    if case > 1:
-        x = np.arange(1, 203)
-        y = np.arange(1, 53)
-    else:
-        x = np.arange(1, 6)
-        y = np.arange(1, 6)
+        X, Y = np.meshgrid(x, y, indexing='xy', sparse=False)
+        u = np.array(u)
+        v = np.array(v)
 
-    X, Y = np.meshgrid(x, y, indexing='xy', sparse=False)
-    u = np.array(u)
-    v = np.array(v)
+        axs[1, 1].pcolor(psi)
+        axs[1, 1].streamplot(X, Y, u, v, density=1, linewidth=.10, arrowsize=.5, arrowstyle='Fancy', cmap='plasma')
+        axs[1, 1].set_title('Stream-function')
 
-    axs[1, 1].pcolor(psi)
-    axs[1, 1].streamplot(X, Y, u, v, density=1, linewidth=.10, arrowsize=.5, arrowstyle='Fancy')
-    axs[1, 1].set_title('Stream-function')
+        axs[2, 1].pcolor(p, cmap='plasma')
+        axs[2, 1].set_title('Pressure')
 
-    axs[2, 1].pcolor(p)
-    axs[2, 1].set_title('Pressure')
-
-    fig.tight_layout()
-    plt.savefig("test.svg")
-
-    plt.show()
+        fig.tight_layout()
+        plt.savefig('cas_numero_' + str(case) + ".eps")
+        plt.show()
+        plt.clf()
